7570be5f24c3092dbd1ac58fd2b18105116dab16
Bohdan Shakhov <bogdanshahov2000@gmail.com>
2021-12-01 16:55:23 +0200

    adding method addMember() to class manager/MembershipManagment

--- src/main/java/com/acolyte/managerapp/manager/MembershipManagment.java
+++ src/main/java/com/acolyte/managerapp/manager/MembershipManagment.java
@@ -1,6 +1,13 @@
 package com.acolyte.managerapp.manager;
 
+import com.acolyte.managerapp.calculator.Calculator;
+import com.acolyte.managerapp.member.Member;
+import com.acolyte.managerapp.member.MultiClubMember;
+import com.acolyte.managerapp.member.SingleClubMember;
+
 import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MembershipManagment {
@@ -30,5 +37,88 @@ public class MembershipManagment {
         System.out.println("Multi Clubs");
     }
 
+    public int getChoise() {
+        int choise = 0;
 
+        System.out.println("WELCOME TO OUR FITNESS CENTER");
+        System.out.println("==============================");
+        System.out.println("1) Add member");
+        System.out.println("2) Remove member");
+        System.out.println("3) Display member information");
+        System.out.println("Please select an option (or Enter -1 to quit)");
+
+        choise = getIntInput();
+        return choise;
+    }
+
+    public String addMembers(LinkedList<Member> members) {
+        String name;
+        int club = 0;
+        String memberInf = null;
+        double fees;
+        int memberID;
+        Member member;
+        Calculator<Integer> calculator;
+
+        //get member`s name
+        System.out.print("Print member`s name: ");
+        name = SCANNER.nextLine();
+
+        //get member`s club
+        while (club < 1 || club > 4) {
+            printClubOptions();
+            club = getIntInput();
+            if (club < 1 || club > 4) {
+                System.err.println("Input correct value!!!");
+            }
+        }
+
+        // get member`s id
+        if (members.size() > 0) {
+            memberID = members.getLast().getMemberID() + 1;
+        } else {
+            memberID = 1;
+        }
+
+        if (club != 4) {
+            calculator = n -> {
+                if (n == 1) {
+                    return 900;
+                } else if (n == 2) {
+                    return 950;
+                } else if (n == 3) {
+                    return 1000;
+                }
+                return -1;
+            };
+            fees = calculator.calculateFees(club);
+
+            member = new SingleClubMember('S', memberID, name, fees, club);
+            members.add(member);
+            memberInf = member.toString();
+            System.out.println("\nSTATUS: Single club member added\n");
+        } else {
+            if (club == 4) {
+                calculator = n -> {
+                    if (n == 4) {
+                        return 1200;
+                    } else {
+                        return -1;
+                    }
+                };
+                fees = calculator.calculateFees(club);
+
+                member = new MultiClubMember('M', memberID, name, fees, 100);
+                members.add(member);
+                memberInf = member.toString();
+                System.out.println("\nSTATUS: Multiclub member added\n");
+            }
+        }
+        return memberInf;
+    }
+
+    public void removeMember(LinkedList<Member> members) {
+        int memberID;
+        System.out.print("Enter ");
+    }
 }
