e94929824d7bc868cf644f60a49d9f7311e9c93c
Bohdan Shakhov <bogdanshahov2000@gmail.com>
2021-12-01 15:01:16 +0200

    adding overwriteFile() method to  filework/FileHandler.java class

--- src/main/java/com/acolyte/managerapp/filework/FileHandler.java
+++ src/main/java/com/acolyte/managerapp/filework/FileHandler.java
@@ -5,10 +5,13 @@ import com.acolyte.managerapp.member.MultiClubMember;
 import com.acolyte.managerapp.member.SingleClubMember;
 
 import java.io.*;
+import java.nio.file.NoSuchFileException;
 import java.util.LinkedList;
 import java.util.List;
 
 public class FileHandler {
+
+    public static final String MEMBERS_CSV = "members.csv";
 
     public List<Member> readFile() {
         List<Member> members = new LinkedList<>();
@@ -16,7 +19,7 @@ public class FileHandler {
         String[] splitLine;
         Member member;
 
-        try(BufferedReader reader = new BufferedReader(new FileReader("members.csv"))) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(MEMBERS_CSV))) {
             lineRead = reader.readLine();
 
             while (lineRead != null) {
@@ -46,11 +49,34 @@ public class FileHandler {
     }
 
     public void appendFile(String member) {
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter("members.csv", true))) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(MEMBERS_CSV, true))) {
             writer.write(member + System.lineSeparator());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void overwriteFile(List<Member> members) {
+        String s;
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("members.temp"))) {
+            for (Member member : members) {
+                s = member.toString();
+                writer.write(s + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            File sourceFile = new File(MEMBERS_CSV);
+            File newFile = new File("members.temp");
+
+            sourceFile.delete();
+            newFile.renameTo(sourceFile);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
