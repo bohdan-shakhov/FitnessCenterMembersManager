b6c7f553e6190e6f4ce0f769bbc004cd304f3f4e
Bohdan Shakhov <bogdanshahov2000@gmail.com>
2021-12-01 13:44:48 +0200

    creating and filling pojo class com/acolyte/managerapp/member/Member.java

---
+++ src/main/java/com/acolyte/managerapp/member/Member.java
@@ -0,0 +1,58 @@
+package com.acolyte.managerapp.member;
+
+public class Member {
+    public static final String LINE_SEPARATOR = System.lineSeparator();
+
+    private char memberType;
+    private int memberID;
+    private String name;
+    private double fees;
+
+    public Member(char memberType, int memberID, String name, double fees) {
+        this.memberType = memberType;
+        this.memberID = memberID;
+        this.name = name;
+        this.fees = fees;
+    }
+
+    public char getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(char memberType) {
+        this.memberType = memberType;
+    }
+
+    public int getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(int memberID) {
+        this.memberID = memberID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getFees() {
+        return fees;
+    }
+
+    public void setFees(double fees) {
+        this.fees = fees;
+    }
+
+    @Override
+    public String toString() {
+        return "Member " + LINE_SEPARATOR +
+                "Type - " + memberType + LINE_SEPARATOR +
+                ", ID - " + memberID + LINE_SEPARATOR +
+                ", name - " + name +  LINE_SEPARATOR +
+                ", fees = " + fees + LINE_SEPARATOR;
+    }
+}
