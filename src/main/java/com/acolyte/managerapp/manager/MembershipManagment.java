57e05cb25117d32c6eedc382d8554d360fb07b92
Bohdan Shakhov <bogdanshahov2000@gmail.com>
2021-12-01 15:20:54 +0200

    creating class manager/MembershipManagment and adding method getIntInput()

---
+++ src/main/java/com/acolyte/managerapp/manager/MembershipManagment.java
@@ -0,0 +1,25 @@
+package com.acolyte.managerapp.manager;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MembershipManagment {
+    private final Scanner SCANNER = new Scanner(System.in);
+
+    private int getIntInput() {
+        int choise = 0;
+
+        while (choise == 0) {
+            try {
+                choise = SCANNER.nextInt();
+                if (choise == 0) {
+                    throw new InputMismatchException();
+                }
+                SCANNER.nextLine();
+            } catch (InputMismatchException e) {
+                System.err.println("INVALID INPUT. Please try again: ");
+            }
+        }
+        return choise;
+    }
+}
